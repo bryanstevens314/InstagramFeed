087f19bbe53d876976b2a1ee22b39efc3ef1f774
Bryan Stevens <bryanstevens@Bryans-MacBook-Pro.local>
2018-09-07 15:55:38 -0700

    getting there

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-let content = document.getElementById("content");
-let testing = false;
-let instragramFeed = {};
+var content = document.getElementById("content");
+var next = document.getElementById("nextPage");
+var prev = document.getElementById("prevPage");
+var testing = false;
+var instragramFeed = {};
 
 
 function loadEverything() {
-    //content.innerHTML = "Hello World";
+
     if (testing === false){
         content.appendChild(propellerBannerAd);
     }
-    //instagramAPI();
+    instagramAPI();
 }
 window.onload = loadEverything;
 
-let params = {
+var params = {
     URL: 'http://instagr.am/p/fA9uwTtkSN/',
     HIDECAPTION: "false",
     OMITSCRIPT: "true",
     CALLBACK:""
 }
-let url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=8521258789.e9bc9a5.cb1b6d0891e84282b2aaa36959ad7a96";
+var url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=8521258789.e9bc9a5.cb1b6d0891e84282b2aaa36959ad7a96";
 function instagramAPI (){
     $.getJSON(url, function (data) {
         instragramFeed = data.data;
-        for (let i = 0; i < 7; i++){
+        for (var i = 0; i < 7; i++){
             var container = document.getElementById('insta-feed');
             var imgURL = instragramFeed[i].images.standard_resolution.url;
-           
             var div = document.createElement('div');
             div.setAttribute('class','instapic');
             container.appendChild(div);
@@ -36,4 +37,11 @@ function instagramAPI (){
 
     });
 }
+function nextPage (){
 
+}
+function prevPage (){
+    
+}
+next.addEventListener('click', nextPage);
+prev.addEventListener('click',prevPage);
