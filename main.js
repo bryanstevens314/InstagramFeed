7519de397247fb0345d83f8e5137a142ad766292
Bryan Stevens <bryanstevens@Bryans-MacBook-Pro.local>
2018-09-28 14:15:24 -0700

    more ads...

--- main.js
+++ main.js
@@ -7,21 +7,13 @@ propellerBannerAd.setAttribute('async', 'async');
 propellerBannerAd.setAttribute('data-cfasync', 'false');
 propellerBannerAd.setAttribute('src', '//native.propellerads.com/1?z=1992444&eid=');
 
-var next = document.createElement('a');
-next.setAttribute = ('id', 'nextPage');
-next.setAttribute = ('class', 'next');
-next.innerHTML = 'Next &raquo;';
-
-var prev = document.createElement('a');
-prev.setAttribute = ('id', 'prevPage');
-prev.setAttribute = ('class', 'previous');
-prev.innerHTML = '&laquo; Previous';
 var instagramFeed = [];
 var clear = false;
 var page = 1;
 var testing = false;
 
 function loadEverything() {
+
     getPageNum(window.location.href);
     if (testing === false) {
         if (page > 1) {
@@ -35,6 +27,7 @@ function loadEverything() {
 window.onload = loadEverything;
 
 function getPageNum(url) {
+
     var id = url.substring(url.lastIndexOf('/') + 1);
     if (url.includes('?')) {
         var arr = id.split('?');
@@ -76,31 +69,50 @@ function instagramAPI() {
                 'fast'
             );
         }
-        var buttonDiv = document.getElementById('buttonDiv1');
-
-        buttonDiv.appendChild(prev);
-        buttonDiv.appendChild(next);
         for (i; i < index; i++) {
-            var imgURL = instagramFeed[i].images.standard_resolution.url;
-            createImageWithURL(imgURL);
-            if (testing === false) {
-                var y = i + 1;
-                if (y % 2 === 0) {
-                    if (i !== 0) {
-                        var div1 = document.createElement('div');
-                        div1.setAttribute('class', 'instapic');
-                        container.appendChild(div1);
-                        var ad = getNativeAd();
-                        div1.appendChild(ad);
+            if (i === 0) {
+                createNavigationDiv();
+            } else {
+                let j = i - 1;
+                var imgURL = instagramFeed[j].images.standard_resolution.url;
+                createImageWithURL(imgURL);
+                if (testing === false) {
+                    var y = j + 1;
+                    if (y % 2 === 0) {
+                        if (j !== 0) {
+                            var div1 = document.createElement('div');
+                            div1.setAttribute('class', 'instapic');
+                            container.appendChild(div1);
+                            var ad = getNativeAd();
+                            div1.appendChild(ad);
+                        }
                     }
                 }
             }
-        }
-        var buttonDiv = document.getElementById('buttonDiv');
 
-        buttonDiv.appendChild(prev);
-        buttonDiv.appendChild(next);
+        }
+        createNavigationDiv();
     });
+}
+
+function createNavigationDiv() {
+    var buttonDiv = document.createElement('div');
+    buttonDiv.setAttribute('id', 'buttonDiv');
+    buttonDiv.setAttribute('class', 'buttons');
+    container.appendChild(buttonDiv);
+    var next = document.createElement('a');
+    next.setAttribute = ('id', 'nextPage');
+    next.setAttribute = ('class', 'next');
+    next.innerHTML = 'Next &raquo;';
+
+    var prev = document.createElement('a');
+    prev.setAttribute = ('id', 'prevPage');
+    prev.setAttribute = ('class', 'previous');
+    prev.innerHTML = '&laquo; Previous';
+    next.addEventListener('click', nextPage);
+    prev.addEventListener('click', prevPage);
+    buttonDiv.appendChild(prev);
+    buttonDiv.appendChild(next);
 }
 
 function createImageWithURL(uri) {
@@ -155,5 +167,3 @@ function prevPage() {
         window.location.replace("./?pageNum=" + page);
     }
 }
-next.addEventListener('click', nextPage);
-prev.addEventListener('click', prevPage);
